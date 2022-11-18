497af4ca9aea102bbc6c77c42931eb52aeef13d9
Daniel Agorander <daniel.agorander@gmail.com>
2022-11-19 00:09:59 +0100

    I have no idea.

--- st-0.8.5/config.h
+++ st-0.8.5/config.h
@@ -5,7 +5,13 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=24:antialias=true:autohint=true";
+// static char *font = "Liberation Mono:pixelsize=24:antialias=true:autohint=true";
+//static char *font = "Jetbrains Mono:pixelsize=24:antialias=true:autohint=true";
+//static char *font = "Fira Mono:pixelsize=24:antialias=true:autohint=true";
+static char *font = "Fantasque Sans Mono:pixelsize=28:antialias=true:autohint=true";
+
+
+
 static int borderpx = 2;
 
 /*
